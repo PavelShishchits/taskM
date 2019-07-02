93b08816da90e489b8fd6f77e787b81fbf8fbaab
PavelBrimit <p.shischits@brimit.com>
2019-07-02 14:40:06 +0300

    swap babel/polyfill to core.js module

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,11 @@ import {psUtils} from './modules/utils';
 import '../components/core/tabs/script';
 
 console.log(psUtils);
+
+const a = new Promise((resolve, reject) => {
+    resolve('mew');
+});
+
+a.then(() => {
+    console.log('mew');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -57,3 +57,6 @@ module.exports = ((env, argv) => {
     ])
   }
 });
+
+// todos
+// toDo include jquery
